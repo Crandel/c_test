3c677a4928ead52edb14fd4fd50c825d971fe578
Crandel <cradlemann@gmail.com>
2017-07-06 21:35:37 +0300

    Add strings

--- string.c
+++ string.c
@@ -3,14 +3,18 @@
 int main(void)
 {
   char name[] = "";
-  int n1 = 0;
-  int n2 = 0;
+  float n1 = 0;
+  float n2 = 0;
   puts("Enter your name");
   scanf("%s", &name);
   printf("Hello, %s\n", name);
   puts("Give me first number");
-  scanf("%d", &n1);
+  scanf("%f", &n1);
   puts("Give me second number");
-  scanf("%d", &n2);
-  printf("%s, your sum is %d\n", name, n1 + n2);
+  scanf("%f", &n2);
+  // at least 3 length and precision is 2
+  printf("%s, %3.2f + %3.2f is %3.2f\n", name, n1, n2, n1 + n2);
+  printf("%s, %3.2f - %3.2f is %3.2f\n", name, n1, n2, n1 - n2);
+  printf("%s, %3.2f * %3.2f is %3.2f\n", name, n1, n2, n1 * n2);
+  printf("%s, %3.2f / %3.2f is %3.2f\n", name, n1, n2, n1 / n2);
 }
