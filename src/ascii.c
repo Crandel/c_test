9d18c1af3fa47d6837f46fb60569712319730634
Crandel <cradlemann@gmail.com>
2017-07-06 23:35:25 +0300

    Improve ascii

--- src/ascii.c
+++ src/ascii.c
@@ -4,6 +4,10 @@ void ascii(void)
 {
   int start = 65;
   for(int i = start; i < start + 26; i++ ){
-    printf("Char %c has code %i\n", (char)i, i);
+    printf("Code %i converted to char %c\n", i, (char)i);
+  }
+  printf("\nReverse convertion\n\n");
+  for(char c = 'A'; c <= 'Z'; c++ ){
+    printf("Char %c has code %i\n", c, (int)c);
   }
 }
