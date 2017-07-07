dd15535dff345ae8a938cca5ec74d094b9168303
Vitalii Drevenchuk <vitalii.drevenchuk@accenture.com>
2017-07-07 11:54:14 +0300

    Test command line args

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <test.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  printf("Run test C program!\n");
+  printf("Run test C program with %i args: ", argc);
+  for (int i = 0; i < argc; i++){
+    printf("%s ", argv[i]);
+  }
+  printf("\n");
   // string_convert();
-  ascii();
+  // ascii();
 }
